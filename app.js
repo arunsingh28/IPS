adbb76943b9bab5f7fa7a6843c1416bd2da64230
arunsingh28 <arunsingh28aug.as@gmail.com>
2020-08-14 14:19:47 +0530

    second

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ app.use(express.static(path.join(__dirname,'/public')));
 
 
 // server listning
-app.listen(5000,()=>{
-    console.log('server running on https://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`server running on https://localhost:${PORT}`);
 })
